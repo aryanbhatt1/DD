d4ef6996a9ec5474fc9c1e2023cb8b98f6b0484f
Aryan Bhatt <aryanbhatt1002@gmail.com>
2022-02-17 14:48:36 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/radiansoftwares/dd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.radiansoftwares.dd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import "package:latlong2/latlong.dart" as latlng;
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: MapOptions(
+        center: latlng.LatLng(51.5, -0.09),
+        zoom: 13.0,
+      ),
+      layers: [
+        TileLayerOptions(
+          urlTemplate: "https://api.mapbox.com/styles/v1/aryanbhatt1002/ckzqm2cpy000b15tguaqzcdqh/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXJ5YW5iaGF0dDEwMDIiLCJhIjoiY2tqaDFmcnloNDFpYTJybnFvbmk0cTliNyJ9.ImW_FZl5b1VBm1bYaLyziA",
+          additionalOptions: {
+            'accessToken': 'pk.eyJ1IjoiYXJ5YW5iaGF0dDEwMDIiLCJhIjoiY2tqaDFmcnloNDFpYTJybnFvbmk0cTliNyJ9.ImW_FZl5b1VBm1bYaLyziA',
+            'id': 'mapbox.mapbox-streets-v8'
+          },
+        ),
+
+      ],
+    );
+  }
+}
